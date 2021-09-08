1f3060104f3958039279517682d3c41a44189a33
CarolLopesFavaretto <carolinelopesrdg@hotmail.com>
2021-09-08 16:40:53 -0300

    Implementação da API

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
+  listasTemas: Tema[]
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == ""){
+      this.router.navigate(["/entar"])
+    }
+
+    this.findAllTemas()
+
   }
+  
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) =>{
+      this.listasTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe ((resp: Tema) => {
+      this.tema = resp
+      alert ("Tema cadastrado com sucesso!")
+      this.findAllTemas ()
+      this.tema = new Tema()
+    })
+  }
+
 
 }
